177e9d704ee1c46518d6eca7c6154edf60a730a4
maksatamanov <maks.atamanov05@mail.ru>
2026-04-15 20:39:08 +0500

    feat: add new dependency (Spring Security)

--- src/main/java/ru/max/SchoolDairy/repository/UserRepository.java
+++ src/main/java/ru/max/SchoolDairy/repository/UserRepository.java
@@ -1,11 +1,8 @@
 package ru.max.SchoolDairy.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Component;
 import ru.max.SchoolDairy.model.User;
-
-import java.util.List;
 
 @Component
 @RepositoryRestResource
